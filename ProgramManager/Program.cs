e239e794b6080d45a477785e41ad924f5b0d8038
tmdrnr12a <tmdrnr12a@naver.com>
2020-09-14 21:00:56 +0900

    초기 커밋.

---
+++ ProgramManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string pName = "ProgramManager";
+
+            Process[] arrProcess = Process.GetProcessesByName(pName);
+            if (arrProcess.Length > 1)
+            {
+                MessageBox.Show($"{pName} Already executed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
